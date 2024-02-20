705803241d5386ba1d7057bc8c9952945fd887f7
michallow <michallo_w@o2.pl>
2024-02-20 08:34:26 +0100

    Upd

--- src/index.js
+++ src/index.js
@@ -3,34 +3,20 @@ import Notiflix from 'notiflix';
 
 // const axios = require('axios').default;
 
-const url = '';
-const genuineQueryString = queryString.split(' ').join('+');
+const API_KEY = '42458788-a8c12bb6253815950dff0c06c';
+const BASE_URL = 'https://pixabay.com/api/';
+let queryString = '';
+const SEARCH_TERM = queryString.split(' ').join('+');
 
-//używaj Pixaby
-//pozyskaj unikalny kod dostępu
-
-// Lista parametrów treści żądania, które należy podać:
-
-// key - Twój unikalny klucz dostępu do API. (42458788-a8c12bb6253815950dff0c06c)
-// q - termin, który chcemy wyszukać. W tej aplikacji to treść którą będzie wpisywał użytkownik.
-// image_type - typ obrazka. Chcemy tylko zdjęcia, dlatego określ wartość parametru jako photo.
-// orientation - orientacja zdjęcia. Określ wartość horizontal.
-// safesearch - wyszukiwanie obrazków SFW (Safe For Work). Określ wartość true.
-
-// W odpowiedzi pojawi się tablica obrazów odpowiadających kryteriom parametrów żądania. Każdy obraz opisany jest obiektem, z którego interesują cię tylko następujące właściwości:
-
-//webformatURL - link do małego obrazka.
-//largeImageURL - link do dużego obrazka.
-//tags - wiersz z opisem obrazka. Będzie pasować do atrybutu alt.
-//likes - liczba “lajków”.
-//views - liczba wyświetleń.
-//comments - liczba komentarzy.
-//downloads - liczba pobrań.
-
-// Jeśli backend przekazuje pustą tablicę, oznacza to, że nic odpowiedniego nie znaleziono. W takim wypadku pokaż powiadomienie o treści "Sorry, there are no images matching your search query. Please try again.". Do powiadomień używaj biblioteki notiflix.
+const photoCard = document.querySelector('.photo-card');
+const searchInput = document.querySelector('[data-search]');
+searchInput.addEventListener('input', event => {
+  queryString = event.target.value.split(' ').join('+');
+  console.log(queryString);
+});
 
 fetch(
-  'https://pixabay.com/api/?key=42458788-a8c12bb6253815950dff0c06c=yellow+flowers&image_type=photo'
+  `${BASE_URL}?key=${API_KEY}&q=${SEARCH_TERM}&image_type=photo&orientation=horizontal&safesearch=true`
 )
   .then(response => response.json())
   .then(({ hits }) => {
@@ -69,45 +55,16 @@ fetch(
       </div>
     `
     );
-
     return markupArray;
   })
   .then(markupArray => {
     const gallery = document.querySelector('.gallery');
-    gallery.innerHTML = markupArray.join('');
+    gallery.innerHTML = ''; // Wyczyść zawartość galerii
+    markupArray.forEach(markup => {
+      gallery.insertAdjacentHTML('beforeend', markup);
+    });
+  })
+  .catch(error => {
+    'Sorry, there are no images matching your search query. Please try again.',
+      error;
   });
-
-// images.hits.map(
-//     ({
-//       webformatURL,
-//       largeImageURL,
-//       tags,
-//       likes,
-//       views,
-//       comments,
-//       downloads,
-//     }) => `
-//   <div class="photo-card">
-//     <a href="${largeImageURL}">
-//       <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//     </a>
-//     <div class="info">
-//       <p class="info-item">
-//         <b>Likes</b>
-//         <br>${likes}
-//       </p>
-//       <p class="info-item">
-//         <b>Views</b>
-//         <br>${views}
-//       </p>
-//       <p class="info-item">
-//         <b>Comments</b>
-//         <br>${comments}
-//       </p>
-//       <p class="info-item">
-//         <b>Downloads</b>
-//         <br>${downloads}
-//       </p>
-//     </div>
-//   </div>`
-//   );
